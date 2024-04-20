5cd8ad5abe2ecd9bcb33aeb839bd049456719aa3
chuyitover <160290054+chuyitover@users.noreply.github.com>
2024-04-20 01:26:03 +0000

    Version 1

--- makefile
+++ makefile
@@ -1,2 +1,5 @@
-bin/tron : src/test,cpp
-	g++ -o bin/tron src/main.cpp -Iinclude -lftxui-screen -lftxui-dom -lftxui-component
+bin/tron : src/main.cpp
+	g++ -o bin/tron src/main.cpp -Iinclude -lftxui-screen -lftxui-dom -lftxui-component -std=c++2a
+
+run : bin/tron
+	./bin/tron
---
+++ src/main.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <ftxui/screen/screen.hpp>
+#include<ftxui/dom/elements.hpp>
+#include <thread>
+#include <fstream>
+using namespace std;
+using namespace ftxui;
+
+int main(int argc, char const *argv[])
+{
+    fstream archivo;
+    archivo.open(",/assets/images/moto.txt");
+    string moto;
+    archivo >> moto;
+    archivo.close();
+
+    int fotograma=0;
+    while (true)
+    {
+    this_thread::sleep_for(0.1s);
+    fotograma++;
+    Element personaje = spinner(21,fotograma) | bold | color(Color::Red) | bgcolor(Color::White);
+    Element eMoto = text(moto);
+    Element dibujo = hbox({personaje, eMoto});
+    Screen pantalla = Screen::Create(Dimension::Full());
+    Render(pantalla, dibujo);
+    pantalla.Print();
+    cout<<pantalla.ResetPosition();
+    }
+return EXIT_SUCCESS; 
+}
